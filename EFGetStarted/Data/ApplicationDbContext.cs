00fae448c81e8f5499b439f310db7394e33bce7f
tomohiro8 <h.kamitomo@me.com>
2021-02-19 00:24:42 +0900

    プロジェクト ファイルを追加します。

---
+++ EFGetStarted/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EFGetStarted.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseNpgsql("Host=localhost;Database=EFGetStarted;Username=postgres;Password=postgres");
+    }
+}
